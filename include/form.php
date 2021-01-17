69251cc416d1b7de5490441f96bd795a21717d58
shyam sedai <shyam.sedai3@gmail.com>
2021-01-16 18:26:33 -0600

    Adding mail functionality

--- include/form.php
+++ include/form.php
@@ -27,8 +27,8 @@ require 'phpmailer/src/SMTP.php';
 $toemails = array();
 
 $toemails[] = array(
-				'email' => 'your-email@website.com', // Your Email Address
-				'name' => 'Your Name' // Your Name
+				'email' => 'shyam.sedai3@gmail.com', // Your Email Address
+				'name' => 'Shyam Sedai' // Your Name
 			);
 
 
@@ -37,8 +37,8 @@ $toemails[] = array(
 ---------------------------------------------------*/
 
 $fromemail = array(
-				'email' => 'no-reply@website.com', // Company's Email Address (preferably currently used Domain Name)
-				'name' => 'Company Name' // Company Name
+				'email' => 'no-reply@nexzenit.com', // Company's Email Address (preferably currently used Domain Name)
+				'name' => 'NexZen IT' // Company Name
 			);
 
 
